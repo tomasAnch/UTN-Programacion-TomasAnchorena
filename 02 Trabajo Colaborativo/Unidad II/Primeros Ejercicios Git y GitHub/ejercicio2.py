abeab8ceddc1a86d56f23990a9eff8507c41327e
Tomas Anchorena <123486949+tomasAnch@users.noreply.github.com>
2025-03-31 18:16:07 -0300

    Agregando carpeta de la unidad 2, Git y GitHub

---
+++ 02 Trabajo Colaborativo/Unidad II/Primeros Ejercicios Git y GitHub/ejercicio2.py
@@ -0,0 +1,7 @@
+#GIT Y GITHUB!!!!!!!!!! 2
+
+nombre = input('Ingresá el nombre: ')
+apellido = input('Ingresá el apellido: ')
+materia = input('Ingresá la materia: ')
+
+print(f"HooOoOolaaaAAAAa soy { nombre } { apellido } y estoy en la materia { materia }")
---
+++ 02 Trabajo Colaborativo/Unidad II/TP°2 - Ejercicio 3/Conflict-Exercise/README.md
@@ -0,0 +1,5 @@
+# Conflict-Exercise
+Ejercicio 3 del TP
+
+Este es un cambio en la main branch.
+Este es un cambio en la feature branch.
